fa7daa47d2ec7d228455eccce66205fff98c7325
ioncakephper <shytiger@yahoo.com>
2022-01-24 14:44:31 +0200

    Initial submit

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { exec }  = require('child_process')
+const program   = require('commander')
+const fileEasy  = require('file-easy')
+const fs        = require('fs')
+const path      = require('path')
+
+program
+    .description('Monitor .hbs file changes and run .js script when changes occur.')
+    .name('hbsmon')
+    .version(require('./package.json').version)
+
+program
+    .arguments('<hbsfile> [jsToRun]')
+    .action((hbsfile, jsToRun, options, commander) => {
+        hbsfile = fileEasy.setDefaultExtension(hbsfile, '.hbs')
+        jsToRun = (!jsToRun) ? path.basename(hbsFile, path.extname(hbsfile)) : jsToRun;
+        jsToRun = fileEasy.setDefaultExtension(jsToRun, '.js')
+    
+        fs.watch(hbsfile, (event, filename) => {
+            if(filename) {
+                exec(`node ${jsToRun}`, (error, stdout, stderr) => {
+                })
+            }
+        })
+    })
+
+program.parse()
